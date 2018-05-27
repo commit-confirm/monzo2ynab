6c4e4cbbac0fe536a1d191a0554d5f861173777f
Kenrick Turner <kenrick@kenners.org>
2018-05-27 14:05:32 +0100

    Reformat code

--- monzo2ynab.py
+++ monzo2ynab.py
@@ -3,10 +3,7 @@ import requests
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
-app.config.update(
-    DEBUG = False,
-    TESTING = False
-)
+app.config.update(DEBUG=False, TESTING=False)
 
 secret_token = ""
 ynab_base = "https://api.youneedabudget.com/v1"
@@ -14,37 +11,45 @@ ynab_api_key = ""
 ynab_budget_id = ""
 ynab_account_id = ""
 
-@app.route('/')
-def root():
-    return 'OK'
 
-@app.route('/ping/{}'.format(secret_token), methods=['POST'])
+@app.route("/")
+def root():
+    return "OK"
+
+
+@app.route("/ping/{}".format(secret_token), methods=["POST"])
 def ping():
     try:
-        payee = request.json['data']['merchant']['name']
+        payee = request.json["data"]["merchant"]["name"]
     except TypeError:
         try:
-            payee = request.json['data']['counterparty']['name']
+            payee = request.json["data"]["counterparty"]["name"]
         except TypeError:
-            payee = request.json['data']['description']
-    
+            payee = request.json["data"]["description"]
+
     transaction = {
         "transaction": {
             "account_id": ynab_account_id,
-            "date": request.json['data']['created'],
-            "amount": (request.json['data']['amount']*10),
+            "date": request.json["data"]["created"],
+            "amount": (request.json["data"]["amount"] * 10),
             "payee_name": payee,
             "cleared": "Cleared",
-            "import_id": "Monzo:{}:{}".format((request.json['data']['amount']), request.json['data']['created'])
+            "import_id": "Monzo:{}:{}".format(
+                (request.json["data"]["amount"]), request.json["data"]["created"]
+            ),
         }
     }
     with requests.Session() as s:
-        s.headers.update({'Authorization': 'Bearer {}'.format(ynab_api_key)})
+        s.headers.update({"Authorization": "Bearer {}".format(ynab_api_key)})
         try:
-            r = s.post('{}/budgets/{}/transactions'.format(ynab_base, ynab_budget_id), json=transaction)
+            r = s.post(
+                "{}/budgets/{}/transactions".format(ynab_base, ynab_budget_id),
+                json=transaction,
+            )
         except requests.exceptions.RequestException as e:
             return e
     return jsonify(r.json())
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run()
